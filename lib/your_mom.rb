4232371f1d2af3afdcba159afc5e428c2cd094c9
Adam Bair <adambair@gmail.com>
2012-02-14 11:22:09 -0500

    Add Ruby functionality to your mom.

--- lib/your_mom.rb
+++ lib/your_mom.rb
@@ -1,1 +1,16 @@
-require 'stupid.rb'
+class YourMom
+  def initialize
+    @path = File.join(File.dirname(__FILE__), "dumb.txt")
+  end
+
+  def is_so_dumb
+    data[rand(data.length)].strip
+  end
+  alias isd is_so_dumb
+
+  def data
+    return @data if @data
+    f = File.open(@path)
+    @data ||= f.readlines
+  end
+end
